3ffacc4a78d2d74afe2e051f94fedbf0a888bf8d
hruthikamunigala <hruthikamunigala05@gmail.com>
2024-06-09 10:54:36 +0530

    updated providers

--- providers.tf
+++ providers.tf
@@ -12,3 +12,15 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
+
+resource "aws_instance" "Dove-Instance" {
+  ami                    = "ami-0d191299f2822b1fa"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"  
+  key_name               = "dove-key"
+  vpc_security_group_ids = ["sg-01ca67672cd0c3177"]
+  tags = {
+    Name    = "Dove-Instance"
+    Project = "Dove"
+  }
+}
