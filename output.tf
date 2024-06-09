4f775395579719e0277759ff26d5d1834cea2840
hruthikamunigala <hruthikamunigala05@gmail.com>
2024-06-09 11:52:28 +0530

    changed id name

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
- value       = aws_instance.public_instance.public_ip
+ value       = aws_instance.Dove-Instance
  description = "Public IP Address of EC2 instance"
 }
 
 output "instance_id" {
- value       = aws_instance.public_instance.id
+ value       = aws_instance.Dove-Instance.id
  description = "Instance ID"
 }
